5e7df5b47184ab75af3c356cda9e5c8de1cb59b0
Tennison Chan <tennison.chan+gh@gmail.com>
2016-08-21 18:06:45 -0400

    Use iframe to contain devise user/signin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  after_action :allow_iframe
   before_action :authenticate_user!
 
+private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :auto_merges, only: [:create]
+  resources :auto_merges, only: [:show, :create, :destroy]
 
   root to: 'home#index'
 end
