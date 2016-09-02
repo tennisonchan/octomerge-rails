dffea2627ea31687c314662dedd2007925509d8c
Tennison Chan <tennison.chan+gh@gmail.com>
2016-09-02 17:10:00 -0400

    Store param iframe before redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   after_action :allow_iframe
+  before_action :store_params
   before_action :authenticate_user!
 
 private
 
   def allow_iframe
     response.headers.except! 'X-Frame-Options'
+  end
+
+  def store_params
+    @iframe = params[:iframe] === '1'
   end
 end
