dc57a0e78209e272ae53aed0f634db35aa75c072
Tennison Chan <tennison.chan+gh@gmail.com>
2016-09-06 08:45:12 -0700

    Set auto_merges dependent on user destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   delegate :pull_requests, :merge_pull_request, to: :client
 
-  has_many :auto_merges
+  has_many :auto_merges, dependent: :destroy
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
