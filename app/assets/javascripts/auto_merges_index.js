27c87644d524a5af0d67ca564f85b8e38ed2d5f4
Tennison Chan <tennison.chan+gh@gmail.com>
2016-09-07 00:43:52 -0700

    Using correct chrome extension id

--- app/assets/javascripts/auto_merges_index.js
+++ app/assets/javascripts/auto_merges_index.js
@@ -6,6 +6,6 @@ window.onload = function() {
       message: 'popup-resize',
       width: dataset.width,
       height: dataset.height
-    }, 'chrome-extension://immoginlcccndojojknbkfkfdbjmecge');
+    }, 'chrome-extension://bcfdlpkdamkbcidkenogbahohlnonebo');
   }
 }
